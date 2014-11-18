234771f9a3e43621e4cda2a87ec14d17ddc19393
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-11-18 01:22:11 -0500

    Use gomon/mongod

--- find_one_test.go
+++ find_one_test.go
@@ -1,28 +1,38 @@
 package query
 
+import "github.com/gomon/mongod"
 import "testing"
-import "gopkg.in/mgo.v2"
 import "gopkg.in/mgo.v2/bson"
 import "github.com/nowk/assert"
 
 func TestFindOneFound(t *testing.T) {
-	mongo(t, func(db *mgo.Database) {
-		var user User
-		users := db.C("users")
-		qry := users.Find(bson.M{"name": "Batman"})
-		found, err := FindOne(qry, &user)
-		assert.True(t, found)
-		assert.Nil(t, err)
-	})
+	m := mongod.New(databasename)
+	db, err := m.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer m.Stop(cleandb)
+
+	var user User
+	users := db.C("users")
+	qry := users.Find(bson.M{"name": "Batman"})
+	found, err := FindOne(qry, &user)
+	assert.True(t, found)
+	assert.Nil(t, err)
 }
 
 func TestFindOneNotFound(t *testing.T) {
-	mongo(t, func(db *mgo.Database) {
-		var user User
-		users := db.C("users")
-		qry := users.Find(bson.M{"name": "Robin"})
-		found, err := FindOne(qry, &user)
-		assert.False(t, found)
-		assert.Nil(t, err)
-	})
+	m := mongod.New(databasename)
+	db, err := m.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer m.Stop(cleandb)
+
+	var user User
+	users := db.C("users")
+	qry := users.Find(bson.M{"name": "Robin"})
+	found, err := FindOne(qry, &user)
+	assert.False(t, found)
+	assert.Nil(t, err)
 }
--- t_test.go
+++ t_test.go
@@ -1,6 +1,5 @@
 package query
 
-import "testing"
 import "gopkg.in/mgo.v2"
 
 type User struct {
@@ -9,26 +8,7 @@ type User struct {
 
 const databasename = "query_test"
 
-func check(t *testing.T, err error) {
-	if err != nil {
-		t.Fatalf("fatal: %s\n", err)
-	}
-}
-
-func mongo(t *testing.T, fn func(*mgo.Database)) {
-	session, err := mgo.Dial("127.0.0.1:27017")
-	check(t, err)
-	defer session.Close()
-
-	db := session.DB(databasename)
-	usersC := db.C("users")
-	err = usersC.Insert(
-		&User{"Batman"},
-		&User{"Cobra Commander"})
-	check(t, err)
-
-	fn(db)
-
-	err = usersC.Remove(nil)
-	check(t, err)
+var cleandb = func(db *mgo.Database) {
+	u := db.C("users")
+	u.Remove(nil)
 }
