9949b1f2622ac43ae56f70ec8bccdb46c5e93a06
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-03-03 23:05:23 -0500

    Refactor Paginate, removes reflection

--- paginate/paginate.go
+++ paginate/paginate.go
@@ -1,23 +1,20 @@
 package paginate
 
 import (
-	"reflect"
-
 	"gopkg.in/mgo.v2"
 )
 
-// Paginate queries all on to d @ Page
-func Paginate(qry *mgo.Query, d interface{}, page *Page) error {
-	var err error
-	qry, err = page.Query(qry)
+// Paginate paginates on query
+func Paginate(qry *mgo.Query, pg *Page, d interface{}) error {
+	qry, err := pg.Query(qry)
 	if err != nil {
 		return err
 	}
-	if err := qry.All(d); err != nil {
+
+	err = qry.All(d)
+	if err != nil {
 		return err
 	}
-
-	page.count = reflect.ValueOf(d).Elem().Len()
 
 	return nil
 }
